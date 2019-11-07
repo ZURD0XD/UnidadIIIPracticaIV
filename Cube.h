ea25685bcd2c3e7861c8fb55c0ba1893ccdf8933
BigBossMonchas <55475303+Big-Boss-Monchas@users.noreply.github.com>
2019-11-06 19:50:08 -0700

    Update Cube.h

--- Cube.h
+++ Cube.h
@@ -5,16 +5,18 @@
 #include "Vertex3d.h"
 #include "Color.h"
 
-// ------- CLASE HIJA / CUBE -----------------------------
+// ------- CLASE HIJA / CUBE --------------------------------
 class Cube : public Shapes3d
 {
 	// ----- ATRIBUTOS PERTENECIENTES A CUBO -------------
 private:
-	Vertex3d vertexPos[8];			// NUMERO DE VERTICES
+	Vertex3d vertexPos[8];				// NUMERO DE VERTICES
 	Color colors[6];				// NUMERO DE COLORES DE CARAS
 	double dimension;				// DIMENSION DEL CUBO
 
 public:
+
+	// ----- CONSTRUCTORES DEL CUBO ---------------------
 	Cube() {
 		setOrigin(0, 0, 0);
 		setColor(1);
@@ -56,6 +58,8 @@ public:
 		calculateVertex();
 	}
 
+	// ----- ESTABLECE LOS COLORES DEPENDIENDO DEL NUMERO DE PARAMETRO --------------------
+	// ENTRA A UN SWITCH QUE ASIGNA UN COLOR A CADA ELEMENTO DEL ARREGLO COLOR
 	void setColor(int colorNumber)
 	{
 		numColor = colorNumber;
@@ -67,40 +71,41 @@ public:
 			   colors[4].setRGB(0.0, 1.0, 0.0);	colors[5].setRGB(0.0, 0.0, 1.0);
 			   break;
 		case 2:
-			   colors[0].setRGB(1.0, 0.0, 1.0);		colors[1].setRGB(1.0, 0.0, 0.0);
+			   colors[0].setRGB(1.0, 0.0, 1.0);	colors[1].setRGB(1.0, 0.0, 0.0);
 			   colors[2].setRGB(0.0, 0.0, 0.75);	colors[3].setRGB(0.0, 1.0, 1.0);
 			   colors[4].setRGB(1.0, 0.75, 0.75);	colors[5].setRGB(0.75, 1.0, 1.0);
 			   break;
 		case 3:
-			   colors[0].setRGB(0.5, 0.5, 0.5);		colors[1].setRGB(0.9, 0.8, 0.7);
-			   colors[2].setRGB(0.6, 0.5, 0.4);		colors[3].setRGB(1.0, 1.0, 0.75);
+			   colors[0].setRGB(0.5, 0.5, 0.5);	colors[1].setRGB(0.9, 0.8, 0.7);
+			   colors[2].setRGB(0.6, 0.5, 0.4);	colors[3].setRGB(1.0, 1.0, 0.75);
 			   colors[4].setRGB(0.75, 1.0, 1.0);	colors[5].setRGB(1.0, 0.75, 1.0);
 			   break;
 		case 4:
-			   for (int i = 0; i < 6; i++) { colors[i].setRGB(1.0, 0.0, 0.0); }
+			   for (int i = 0; i < getNumVertex() - 2; i++) { colors[i].setRGB(1.0, 0.0, 0.0); }
 			   break;
 		case 5:
-			   for (int i = 0; i < 6; i++)	{ colors[i].setRGB(0.0, 1.0, 0.0); }
+			   for (int i = 0; i < getNumVertex() - 2; i++)	{ colors[i].setRGB(0.0, 1.0, 0.0); }
 			   break;
 		case 6:
-			   for (int i = 0; i < 6; i++) { colors[i].setRGB(0.0, 0.0, 1.0); }
+			   for (int i = 0; i < getNumVertex() - 2; i++) { colors[i].setRGB(0.0, 0.0, 1.0); }
 			   break;
 		case 7:
-			   for (int i = 0; i < 6; i++) { colors[i].setRGB(1.0, 1.0, 0.0); }
+			   for (int i = 0; i < getNumVertex() - 2; i++) { colors[i].setRGB(1.0, 1.0, 0.0); }
 			   break;
 		case 8:
-			   for (int i = 0; i < 6; i++) { colors[i].setRGB(1.0, 0.0, 1.0); }
+			   for (int i = 0; i < getNumVertex() - 2; i++) { colors[i].setRGB(1.0, 0.0, 1.0); }
 			   break;
 		case 9:
-			   for (int i = 0; i < 6; i++) { colors[i].setRGB(0.0, 1.0, 1.0); }
+			   for (int i = 0; i < getNumVertex() - 2; i++) { colors[i].setRGB(0.0, 1.0, 1.0); }
 			   break;
 		default:
 			numColor = 0;
-			for (int i = 0; i < 6; i++) { colors[i].setRGB(0.0, 0.0, 0.0); }
+			for (int i = 0; i < getNumVertex() - 2; i++) { colors[i].setRGB(0.0, 0.0, 0.0); }
 			break;
 		}
 	}
-
+	
+	// ----- SETTERS & GETTERS ----------------------------------
 	void setDimension(double _dimension)
 	{
 		if (_dimension > 0) { dimension = _dimension; }
@@ -121,14 +126,16 @@ public:
 	double calculateArea() { return (getDimension() * getDimension()) * 6; }
 	double calculateVolume() { return (getDimension() * getDimension() * getDimension()); }
 
+	// ----- ASIGNA LAS COORDENADAS EN X, Y, Z EN CADA VERTICE DEL CUBO ---------------------
 	void setVertex(double _x1, double _y1, double _z1, double dim)
 	{
-		vertexPos[0].setXYZ(_x1, _y1, _z1);						vertexPos[1].setXYZ(_x1, _y1, _z1 + dim);
-		vertexPos[2].setXYZ(_x1, _y1 + dim, _z1 + dim);			vertexPos[3].setXYZ(_x1, _y1 + dim, _z1);
-		vertexPos[4].setXYZ(_x1 - dim, _y1, _z1);				vertexPos[5].setXYZ(_x1 - dim, _y1, _z1 + dim);
+		vertexPos[0].setXYZ(_x1, _y1, _z1);			vertexPos[1].setXYZ(_x1, _y1, _z1 + dim);
+		vertexPos[2].setXYZ(_x1, _y1 + dim, _z1 + dim);		vertexPos[3].setXYZ(_x1, _y1 + dim, _z1);
+		vertexPos[4].setXYZ(_x1 - dim, _y1, _z1);		vertexPos[5].setXYZ(_x1 - dim, _y1, _z1 + dim);
 		vertexPos[6].setXYZ(_x1 - dim, _y1 + dim, _z1 + dim);	vertexPos[7].setXYZ(_x1 - dim, _y1 + dim, _z1);
 	}
 
+	// ----- CALCULA LAS COORDENADAS DEL VERTICE INICIAL PARTIENDO DEL CENTRO DEL CUBO COMO ORIGEN -------
 	void calculateVertex()
 	{
 		Vertex3d aux_origin = getOrigin();
@@ -136,11 +143,13 @@ public:
 		double x1 = aux_origin.getX() + (aux_dimension / 2);
 		double y1 = aux_origin.getY() - (aux_dimension / 2);
 		double z1 = aux_origin.getZ() - (aux_dimension / 2);
-		setVertex(x1, y1, z1, aux_dimension);
+		setVertex(x1, y1, z1, aux_dimension);	// LLAMA LA ASIGNACION DE LOS VERTICES
 	}
-
+	
+	// DIBUJA LA FIGURA PINTANDO CADA UNA DE LAS CARAS CON DIFERENTE COLOR
 	void drawFigure()
 	{
+		// ----- POR CADA CARA TOMA EL COLOR DEL ARREGLO ---------------------
 		Color aux_color = colors[0];
 		glColor3f(aux_color.getR(), aux_color.getG(), aux_color.getB());
 		glBegin(GL_POLYGON);
