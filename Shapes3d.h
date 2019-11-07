56fdc312c7a027516a798ffd6592ff911f7351ff
BigBossMonchas <55475303+Big-Boss-Monchas@users.noreply.github.com>
2019-11-06 19:51:35 -0700

    Update Shapes3d.h

--- Shapes3d.h
+++ Shapes3d.h
@@ -3,7 +3,7 @@
 #include <GL/glut.h>
 #include "Vertex3d.h"
 
-// ------- CLASE PRINCIPAL ----------------------------------
+// ------- CLASE PRINCIPAL SHAPES3D--------------------------------
 class Shapes3d
 {
 	// ----- ATRIBUTOS DE LAS FIGURAS -------------------------
@@ -20,6 +20,9 @@ protected:
 	// EN LA CLASE HIJA --------------------------------------
 public:
 
+	// GETTERS & SETTERS--------------------------------------
+	
+	// ESTABLECE EL ORIGEN DE LA FIGURA
 	void setOrigin(double _x, double _y, double _z)
 	{
 		origin.setX(_x);
@@ -32,16 +35,16 @@ public:
 	Vertex3d getOrigin() const { return origin; }
 	int getNumColor() const { return numColor; }
 	int getNumVertex() const { return numVertex; }
-  double getPerimeter() const { return perimeter; }
+	double getPerimeter() const { return perimeter; }
 	double getArea() const { return area; }
 	double getVolume() const { return volume; }
 
-	// --- METODOS DE LAS FIGURAS ------------------------
-	virtual double calculateArea() = 0;
-	virtual double calculatePerimeter() = 0;
-	virtual double calculateVolume() = 0;
-	virtual void assignProperties() = 0;
-	virtual void drawFigure() = 0;
-	virtual void moveFigure() = 0;
-	virtual void scaleFigure() = 0;
+	// --- METODOS DE LAS FIGURAS ---------------------------
+	//virtual double calculateArea() = 0;
+	//virtual double calculatePerimeter() = 0;
+	//virtual double calculateVolume() = 0;
+	//virtual void assignProperties() = 0;
+	//virtual void drawFigure() = 0;
+	//virtual void moveFigure() = 0;
+	//virtual void scaleFigure() = 0;
 };
